cd2016e9da01174d85cd5511f31a23805644dd01
ProSavage <naman0311@gmail.com>
2022-07-22 18:53:40 -0500

    Formatting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,45 +1,53 @@
-import Document from 'next/document';
-import React from 'react';
-import { ServerStyleSheet } from 'styled-components';
+import Document from "next/document";
+import React from "react";
+import { ServerStyleSheet } from "styled-components";
 import { Head, Main, NextScript, Html } from "next/document";
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
-                });
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-            const initialProps = await Document.getInitialProps(ctx);
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        <link rel={"stylesheet"} href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap'/>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            };
-        } finally {
-            sheet.seal();
-        }
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            <link
+              rel={"stylesheet"}
+              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
+            />
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
     }
+  }
 
-
-    render() {
-        return <Html>
+  render() {
+    return (
+      <Html>
         <Head>
-          <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+          <script
+            async
+            defer
+            src="https://scripts.simpleanalyticscdn.com/latest.js"
+          ></script>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    }
+    );
+  }
 }
